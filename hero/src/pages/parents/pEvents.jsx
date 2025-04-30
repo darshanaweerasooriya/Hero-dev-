5f2c383fc1125802186f0211d3703b9885b28801
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-05-01 03:35:36 +0530

    All pages of Parent side done

---
+++ hero/src/pages/parents/pEvents.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./friendRequest.css";
+
+function PEvents() {
+    const [requests, setRequests] = useState([]);
+    
+        useEffect(() => {
+            // Dummy data for now; you can replace with actual API later
+            setRequests([
+                "Science Fair",
+                "Mini Project I",
+                "MAths Exhibitions",
+             
+            ]);
+        }, []);
+    
+        return (
+            <div className="request-container">
+                <ul className="list-group">
+                    {requests.map((name, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                            <div className="d-flex align-items-center gap-3">
+                                <div className="circle-avatar"></div>
+                                <span>{name}</span>
+                            </div>
+                            <div className="d-flex gap-2">
+                                <button className="btn btn-accept">View</button>
+                                <button className="btn btn-decline bg-red">Leave</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+}
+
+export default PEvents;
